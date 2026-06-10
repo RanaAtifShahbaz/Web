6816a85596d616218371fdbfe72d731d5c0c4342
RanaAtifShahbaz <ranaatif.dev@gmail.com>
2026-06-10 03:49:39 -0400

    Hoisting

---
+++ Hoisting in JS/script.js
@@ -0,0 +1,6 @@
+//Hoisting is a JavaScript mechanism where variables and function declarations are moved to the top of their containing scope during the compilation phase. This means that you can use variables and functions before they are declared in the code.
+// Example 1
+
+console.log(a);
+var a = 10;
+console.log(a);
