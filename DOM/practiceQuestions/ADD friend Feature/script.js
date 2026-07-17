41408007e2d0a29dad30298dd57e6f0a66051550
RanaAtifShahbaz <ranaatif.dev@gmail.com>
2026-07-17 22:40:00 +0500

    Friend Add

---
+++ DOM/practiceQuestions/ADD friend Feature/script.js
@@ -0,0 +1,6 @@
+var isStatus=document.querySelector("h5")
+var addFriend=document.querySelector("#add")
+addFriend.addEventListener("click",()=>{
+    isStatus.innerHTML="Friends"
+    isStatus.style.color="green"
+})
