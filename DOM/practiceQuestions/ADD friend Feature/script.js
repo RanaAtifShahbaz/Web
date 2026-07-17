7d54f6b0671b8b65ca38b773cf930ffa21bc0c76
RanaAtifShahbaz <ranaatif.dev@gmail.com>
2026-07-17 23:12:28 +0500

    Only one button for add and remove

--- DOM/practiceQuestions/ADD friend Feature/script.js
+++ DOM/practiceQuestions/ADD friend Feature/script.js
@@ -1,11 +1,26 @@
 var isStatus=document.querySelector("h5")
-var addFriend=document.querySelector("#add")
-addFriend.addEventListener("click",()=>{
+var btn=document.querySelector("#add")
+let flag=0;
+btn.addEventListener("click",()=>{
+    if(flag==0){
     isStatus.innerHTML="Friends"
     isStatus.style.color="green"
-})
-var remove=document.querySelector("#remove")
-remove.addEventListener("click",()=>{
+    btn.innerHTML="Remove"
+    flag=1;
+
+              
+}
+else{
     isStatus.innerHTML="Stranger"
     isStatus.style.color="red"
+    btn.innerHTML="Add Friend"
+
+    flag=0;
+}
+
 })
+// var remove=document.querySelector("#remove")
+// remove.addEventListener("click",()=>{
+//     isStatus.innerHTML="Stranger"
+//     isStatus.style.color="red"
+// })
